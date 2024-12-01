9edfd9ddecb0d25b4b8670915fe5062988017b0d
0scarRickovic <Med710scarrickovic@gmail.com>
2024-12-01 18:58:03 +0100

    new folder overide function

--- cloud-frontend/src/Componenets/ComonentsJs/Home.js
+++ cloud-frontend/src/Componenets/ComonentsJs/Home.js
@@ -1,37 +1,58 @@
 import { useState } from "react";
 import { FileManager } from "@cubone/react-file-manager";
 import "@cubone/react-file-manager/dist/style.css";
-import "../ComponentsCss/Home.css"
+import "../ComponentsCss/Home.css";
 import Terminal from "./Terminal";
 
 function Home() {
   const [files, setFiles] = useState([
     {
       name: "Documents",
-      isDirectory: true, // Folder
-      path: "/Documents", // Located in Root directory
-      updatedAt: "2024-09-09T10:30:00Z", // Last updated time
+      isDirectory: true,
+      path: "/Documents",
+      updatedAt: "2024-09-09T10:30:00Z",
     },
     {
       name: "Pictures",
       isDirectory: true,
-      path: "/Pictures", // Located in Root directory as well
+      path: "/Pictures",
       updatedAt: "2024-09-09T11:00:00Z",
     },
     {
       name: "Pic.png",
-      isDirectory: false, // File
-      path: "/Pictures/Pic.png", // Located inside the "Pictures" folder
+      isDirectory: false,
+      path: "/Pictures/Pic.png",
       updatedAt: "2024-09-08T16:45:00Z",
-      size: 2048, // File size in bytes (example: 2 KB)
+      size: 2048,
     },
+    {
+        name: "Pic2.png",
+        isDirectory: false,
+        path: "/Pictures/Pic2.png",
+        updatedAt: "2024-09-08T16:45:00Z",
+        size: 2048,
+      },
   ]);
 
+  // Custom function to handle folder creation
+  const handleCreateFolder = (newFolderName, parentPath) => {
+    console.log(parentPath)
+    const newFolder = {
+      name: newFolderName,
+      isDirectory: true,
+      path: `${parentPath.path}/${newFolderName}`,
+      updatedAt: new Date().toISOString(),
+    };
+    console.log("Created Folder Data:", newFolder);
+    setFiles((prevFiles) => [...prevFiles, newFolder]);
+    console.log(files)
+  };
+
   return (
-      <>
-        <FileManager files={files}/>
-        <Terminal/>
-      </>
+    <>
+      <FileManager files={files} onCreateFolder={handleCreateFolder} />
+      <Terminal />
+    </>
   );
 }
 
