5ae4d209a16f5da600575fd9c3bb628603cc16e4
oscarRickovic <Med710scarrickovic@gmail.com>
2024-12-15 22:25:31 +0100

    change Storage location

--- cloud/database/Statics.js
+++ cloud/database/Statics.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 class Statics {
-    static STORAGE_DIR = path.join(__dirname, "..", "..", "Storage")
+    static STORAGE_DIR = path.join(__dirname, "..", "Storage")
 }
 
 module.exports = Statics;
